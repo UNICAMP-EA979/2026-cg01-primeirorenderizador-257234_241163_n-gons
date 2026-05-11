5aacf7bf1b7c6b4aad171fb76d1f0861b58c6230
Julio Nunes Avelar <jn5.guape@gmail.com>
2026-05-11 03:01:58 -0300

    Implementando entrypoints
    Implementando entrypoint 02 com piramide; 03 com objetos filhos; 04 com
    falha do algoritmo do pintor, 05 com animacão giratoria do CUBO.
    
    Atualizando TODO do README.md

--- entrypoints/03-grandchild.py
+++ entrypoints/03-grandchild.py
@@ -14,10 +14,35 @@ if __name__ == "__main__":
     renderer = urenderer.renderer.PyplotRenderer(1920, 1080)
     runtime = urenderer.application.Runtime(renderer, name="03-grandchild")
 
-    # Crie a cena
+    objeto0 = urenderer.node.Node()
+    objeto1 = urenderer.node.Node()
+    objeto2 = urenderer.node.Node()
+
+    objeto0.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    objeto1.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+    objeto2.render_data = urenderer.geometry.polygonal_ifs.get_ifs_cube()
+
+    # O nó avô fica mais afastado da câmera
+    objeto0.translation = np.array([0, 0, -6], dtype=np.float64)
+
+    # O filho é deslocado em relação ao pai
+    objeto1.translation = np.array([1.5, 0, 0], dtype=np.float64)
+
+    # O neto é deslocado em relação ao filho
+    objeto2.translation = np.array([1.5, 0, 0], dtype=np.float64)
+
+    #objeto0.scale = np.array([0.5, 0.5, 0.5], dtype=np.float64)
+    #objeto1.scale = np.array([0.5, 0.5, 0.5], dtype=np.float64)
+    #objeto2.scale = np.array([0.5, 0.5, 0.5], dtype=np.float64)
+
+    objeto0.add_child(objeto1)
+    objeto1.add_child(objeto2)
+
+    runtime.scene.add_child(objeto0)
 
     runtime.iter(capture=True)
 
     # Rotacione o nó avô
+    objeto0.rotation = np.array([0, 45, 0], dtype=np.float64)
 
     runtime.iter(capture=True)
